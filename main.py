6c1f0549a3389a108b37e1839b4ba69ac99602ca
Kurision <kumalanil34@gmail.com>
2023-06-12 19:27:44 +0545

    Completed

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 api_key = "2f9ec8b17725ce7d200b06b69a98ade8"  
 
-
 @app.get("/")
 
 async def read_root(request: Request):
-    # Fetch popular movies from the API
         async with aiohttp.ClientSession() as session:
             url = f"https://api.themoviedb.org/3/movie/popular?api_key={api_key}"
             async with session.get(url) as response:
                 data = await response.json()
-
-        movies = data.get("results", [])  # Extract the movie list from the API response
-        print(movies)
+        movies = data.get("results", []) 
+        # print(movies)
         return templates.TemplateResponse("index.html", {"request": request, "movies": movies})
 
